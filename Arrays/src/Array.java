7246c1b75111e6e97daa466985b77dc8fded677e
ShAd0W20 <shadow.jocs@gmail.com>
2021-10-05 18:42:47 +0200

    Arrays

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 public class Array {
 
 	public static void main(String[] args) throws InterruptedException {
-		String[][] shit = { { "Audi", "BMW", "Aston Martin", "Ferrari", "Lambo" }, { "Suka", "Blyat", "Rush", "Aaaaa" } };
+		String[][] shit = { { "Audi", "BMW", "Aston Martin", "Ferrari", "Lambo" },
+				{ "Suka", "Blyat", "Rush", "Aaaaa", "Blya" } };
 		String[] cars = { "Audi", "BMW", "Aston Martin", "Ferrari", "Lambo" };
 		ArrayList<String> numbers = new ArrayList<String>();
-		
+		int[] primary = new int[3];
+		int[] secondary = { 1, 2, 3 };
+
 		for (int u = 0; u < shit.length; ++u) {
 			for (int j = 0; j < shit[u].length; ++j) {
 				Thread.sleep(100);
@@ -18,7 +21,6 @@ public class Array {
 			Thread.sleep(100);
 			System.out.println(i);
 		}
-
 
 		numbers.add("One");
 		numbers.add("Two");
@@ -32,6 +34,19 @@ public class Array {
 				e.printStackTrace();
 			}
 		});
+
+		primary[0] = 1;
+		primary[1] = 2;
+		primary[2] = 3;
+
+		for (int i : primary) {
+			System.out.println(i);
+		}
+
+		
+		for (int j : secondary) {
+			System.out.println(j);
+		}
 
 	}
 
