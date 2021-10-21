4678e99a308407c2e7944458f2c575b031bde2bb
ShAd0W20 <shadow.jocs@gmail.com>
2021-10-21 11:12:20 +0200

    Code optimization

--- P1_Gestion_Videojuegos/src/GestionVideojuegos.java
+++ P1_Gestion_Videojuegos/src/GestionVideojuegos.java
@@ -11,109 +11,99 @@ public class GestionVideojuegos {
 
 		int totalGamesInArray = 0; // integer to know how many games have we stored in the arrays
 
-		String menuOption = ""; // string to know what's the user option
+		String menuOption = ""; // string to know what's the user option in the main menu
 		String listGameCategory = "";  // String used to difference between gameCategory boolean
-		String userChangeOption = ""; //String variable to know value to change
 		String userInputGameName = ""; // String variable to know what game user whant's to update
+		char userChangeOption = '0'; //char variable to know what value to change
+		char userInputGameCategory = '0'; //char variable to get user input new game category
 
 		System.out.println("Introduce que desea hacer: \n 1 - Guardar un juego \n 2 - Modificar datos de un juego \n 3 - Lista de todos los juegos \n 'Salir' para salir del programa");
 		do {
 			menuOption = input.nextLine(); // Assign user input to variable 'menuOption' in order to enter the different menu options
 
 			switch (menuOption) { // switch main menu
-			case "1": // If user entered 1 in option menu enter this case and register a new game
-				if (totalGamesInArray == 5) { // If array is full exit
-					System.out.println("No puedes guardar mas juegos");
-				} else {
-					System.out.println("Introduce el nombre del juego a guardar"); // ask for game name
-					gameName[totalGamesInArray] = input.nextLine(); // store user input game name to array index with value of totalGamesInArray (first input should be  totalGamesInArray = 0)
-
-					System.out.println("Introduce el precio del juego a guardar"); // ask for game price
-					gamePrice[totalGamesInArray] = input.nextDouble();// store user input game price to array index with value of totalGamesInArray
-					input.nextLine();
-
-					System.out.println("Introduce \n true si es +18 \n false si es -18"); // ask if game is +18 or not
-					gameCategory[totalGamesInArray] = input.nextBoolean(); // store user input game category to array index with value of totalGamesInArray
-					input.nextLine();
-					
-					if (gameCategory[totalGamesInArray] == true) {
-						listGameCategory = "+18";
+				case "1": // If user entered 1 in option menu enter this case and register a new game
+					if (totalGamesInArray == 5) { // If array is full exit
+						System.out.println("No puedes guardar mas juegos");
 					} else {
-						listGameCategory = "-18";
+						System.out.println("Introduce el nombre del juego a guardar"); // ask for game name
+						gameName[totalGamesInArray] = input.nextLine(); // store user input game name to array index with value of totalGamesInArray (first input should be  totalGamesInArray = 0)
+	
+						System.out.println("Introduce el precio del juego a guardar"); // ask for game price
+						gamePrice[totalGamesInArray] = input.nextDouble();// store user input game price to array index with value of totalGamesInArray
+						input.nextLine();
+	
+						System.out.println("Introduce \n 'S' si es +18 \n 'N' si es -18"); // ask if game is +18 or not
+						userInputGameCategory = input.nextLine().charAt(0); // store user input game category
+						
+						gameCategory[totalGamesInArray] = (Character.toLowerCase(userInputGameCategory) == 's') ? true : false; // if userInputGameCategory variable == s set boolean gameCategory with index of totalGamesInArray to TRUE else FALSE
+						
+						listGameCategory = (gameCategory[totalGamesInArray] == true) ? "+18" : "-18"; // if gameCategory index is true set listGameCateogry variable == +18 else set it to -18
+						
+						System.out.println(gameName[totalGamesInArray] + " " + gamePrice[totalGamesInArray] + " " + listGameCategory + " : Videojuego guardado"); // print game data stored into the arrays
+	
+						totalGamesInArray++; // plus 1 to totalGamesInArray
+						System.out.println("\n Actualmente tienes " + totalGamesInArray + " juegos guardados \n\n Pulse enter para salir al menu."); // print the total games stored
 					}
-					System.out.println(gameName[totalGamesInArray] + " " + gamePrice[totalGamesInArray] + " " + listGameCategory + " : Videojuego guardado"); // print game data stored into the arrays
-
-					totalGamesInArray++; // plus 1 to totalGamesInArray
-					System.out.println("Actualmente tienes " + totalGamesInArray + " juegos guardados \n Pulse enter para salir al menu."); // print the total games stored
-				}
-				break;
-			case "2":
-				System.out.println("Introduce el nombre del juego a modificar"); //ask user what game whant's to change
-				userInputGameName = input.nextLine(); //store user input into the variable
-				
-				int i = 0; // integer used to know array index equals to user input
-				while(!userInputGameName.equals(gameName[i]) && i < totalGamesInArray) {// loop throw total games in array while user input not equals game name					
-					i++;
-				}
-				if(userInputGameName.equals(gameName[i])) { // if finds game name equals to user input do:
+					break;
+				case "2":
+					System.out.println("Introduce el nombre del juego a modificar"); //ask user what game whant's to change
+					userInputGameName = input.nextLine(); //store user input into the variable
 					
-					System.out.println("Que valor quieres modificar? \n 1: Precio \n 2: Categoria"); // ask what value to change
-					
-					userChangeOption = input.nextLine(); // store user input into the variable
-					
-					switch(userChangeOption) { // switch between user input value
-						case "1": // case user input == 1 change game price
-							System.out.println("Introduce el nuevo precio para " + gameName[i] + " actualmente con precio: " + gamePrice[i]); // ask the new price for the game
-							gamePrice[i] = input.nextDouble(); // store the new price into gamePrice array with index of the gameName
-							input.nextLine();
-							
-							if (gameCategory[i] == true) {
-								listGameCategory = "+18";
-							} else {
-								listGameCategory = "-18";
-							}
-							break;
-						case "2": // case user input == 1 change game category
-							System.out.println("Introduce la nueva categorya para " + gameName[i] + "\n ture: +18 \n false: -18"); // ask the new category for the game
-							gameCategory[i] = input.nextBoolean(); // store the new category into gameCategory array with index of the gameName
-							input.nextLine();
-							
-							if (gameCategory[i] == true) {
-								listGameCategory = "+18";
-							} else {
-								listGameCategory = "-18";
-							}
-							break;
+					int i = 0; // integer used to know array index equals to user input
+					while(!userInputGameName.toLowerCase().equals(gameName[i].toLowerCase()) && i < totalGamesInArray) {// loop throw total games in array while user input not equals game name					
+						i++;
 					}
-					
-					System.out.println(gameName[i] + " " + gamePrice[i] + " " + listGameCategory + " : Videojuego actualizado \n Pulse enter para salir la menu"); // print the new values of the game	
-				} else {
-					System.out.println("El juego " + userInputGameName + " no existe \n Pulse enter para salir al menu.");					
-				}
-				
-				break;
-			case "3":
-				System.out.println("Actualmente tienes " + totalGamesInArray + " juegos registrados \n "); // print the total games stored in the array
-
-				for (int j = 0; j < totalGamesInArray; j++) { // for loop throw totalGamesInArray
-					if (gameCategory[j] == true) {
-						listGameCategory = "+18";
+					if(userInputGameName.toLowerCase().equals(gameName[i].toLowerCase())) { // if finds game name equals to user input do:
+						
+						System.out.println("Que valor quieres modificar? \n 1: Precio \n 2: Categoria"); // ask what value to change
+						
+						userChangeOption = input.nextLine().charAt(0); // store user input into the variable
+						
+						switch(userChangeOption) { // switch between user input value
+							case '1': // case user input == 1 change game price
+								System.out.println("Introduce el nuevo precio para " + gameName[i] + " actualmente con precio: " + gamePrice[i]); // ask the new price for the game
+								gamePrice[i] = input.nextDouble(); // store the new price into gamePrice array with index of the gameName
+								input.nextLine();
+								
+								break;
+							case '2': // case user input == 1 change game category
+								System.out.println("Introduce la nueva categorya para " + gameName[i] + "\n 'S' si es +18 \n 'N' si es -18"); // ask the new category for the game
+								userInputGameCategory = input.nextLine().charAt(0); // store user input game category to array index with value of totalGamesInArray
+								
+								gameCategory[i] = (Character.toLowerCase(userInputGameCategory) == 's') ? true : false; // if userInputGameCategory variable = s set boolean gameCategory to TRUE else FALSE
+								
+								break;
+						}
+						
+						listGameCategory = (gameCategory[i] == true) ? "+18" : "-18"; // if gameCategory index is true set listGameCateogry variable = +18 else set it to -18
+						
+						System.out.println(gameName[i] + " " + gamePrice[i] + " " + listGameCategory + " : Videojuego actualizado \n Pulse enter para salir la menu"); // print the new values of the game	
 					} else {
-						listGameCategory = "-18";
+						System.out.println("El juego " + userInputGameName + " no existe \n Pulse enter para salir al menu."); // if user input a non existing game print game does not exists
 					}
-					System.out.println(gameName[j] + " / " + gamePrice[j] + " / " + listGameCategory); // Print the list of games stored in the array
-				}
-				System.out.println("\n Pulse enter para salir al menu.");
-				break;
-			default:
-				if(!menuOption.toLowerCase().equals("salir")) {
-					System.out.println("Introduce que desea hacer: \n 1 - Guardar un juego \n 2 - Modificar datos de un juego \n 3 - Lista de todos los juegos \n 'Salir' para salir del programa");
-				}
+					
+					break;
+				case "3":
+					System.out.println("Actualmente tienes " + totalGamesInArray + " juegos registrados \n "); // print the total games stored in the array
+	
+					for (int j = 0; j < totalGamesInArray; j++) { // for loop throw totalGamesInArray
+						
+						listGameCategory = (gameCategory[j] == true) ? "+18" : "-18"; // if gameCategory index is true set listGameCateogry variable = +18 else set it to -18
+						
+						System.out.println(gameName[j] + " / " + gamePrice[j] + " / " + listGameCategory); // Print the list of games stored in the array
+					}
+					System.out.println("\n Pulse enter para salir al menu.");
+					break;
+				default:
+					if(!menuOption.toLowerCase().equals("salir")) {
+						System.out.println("Introduce que desea hacer: \n 1 - Guardar un juego \n 2 - Modificar datos de un juego \n 3 - Lista de todos los juegos \n 'Salir' para salir del programa");
+					}
 			}
 
 		} while (!menuOption.toLowerCase().equals("salir"));
 
-		System.out.println("Has salido del programa");
 		input.close();
+		System.out.println("Has salido del programa");
 	}
 }
