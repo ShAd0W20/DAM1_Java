ab19febeac0fcf39779128bc324bba868374c4bb
ShAd0W20 <shadow.jocs@gmail.com>
2021-11-23 18:22:44 +0100

    Three in a row

---
+++ 3EnRaya/src/TresEnRaya.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TresEnRaya {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		Random rnd = new Random();
+
+		final int boardHeight = 10;
+		final int boardWidth = 4;
+
+		char[][] board = new char[boardWidth][boardHeight];
+
+		int userInput = 0;
+		int computerInput = 0;
+		int position = 0;
+		int position2 = 0;
+
+		do {
+			System.out.println("A que columna desea tirar");
+
+			userInput = input.nextInt();
+			input.nextLine();
+
+			computerInput = rnd.nextInt(4);
+
+			if (Character.toLowerCase(board[userInput - 1][position]) != '\0') {
+				board[userInput - 1][position + 1] = 'O';
+				position++;
+			} else {
+				board[userInput - 1][0] = 'O';
+			}
+			
+			if (Character.toLowerCase(board[computerInput][position2]) != '\0') {
+				board[computerInput][position2 + 1] = 'X';
+				position2++;
+			} else {
+				board[computerInput][0] = 'X';
+			}
+			
+
+			input.close();
+
+			for (int i = 9; i >= 0; i--) {
+				for (int j = 3; j >= 0; j--) {
+					if (Character.toLowerCase(board[j][i]) == '\0') {
+						System.out.print(" _ ");
+					} else if (Character.toLowerCase(board[j][i]) == 'o') {
+						System.out.print(" O ");
+					} else if (Character.toLowerCase(board[j][i]) == 'x') {
+						System.out.print(" X ");
+					}
+				}
+				System.out.println();
+			}
+		} while (true);
+		
+
+	}
+
+}
