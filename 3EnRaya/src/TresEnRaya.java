052bfaa5731f1209667e500003f0150cb3e1c350
ShAd0W20 <shadow.jocs@gmail.com>
2021-11-25 15:07:27 +0100

    Still don't work at all

--- 3EnRaya/src/TresEnRaya.java
+++ 3EnRaya/src/TresEnRaya.java
@@ -26,14 +26,14 @@ public class TresEnRaya {
 
 			computerInput = rnd.nextInt(4);
 
-			if (Character.toLowerCase(board[userInput - 1][position]) != '\0') {
+			if (Character.toLowerCase(board[userInput - 1][position]) != '\0' && Character.toLowerCase(board[computerInput][position2]) != 'X') {
 				board[userInput - 1][position + 1] = 'O';
 				position++;
 			} else {
 				board[userInput - 1][0] = 'O';
 			}
 			
-			if (Character.toLowerCase(board[computerInput][position2]) != '\0') {
+			if (Character.toLowerCase(board[computerInput][position2]) != '\0' && Character.toLowerCase(board[computerInput][position2]) != 'O') {
 				board[computerInput][position2 + 1] = 'X';
 				position2++;
 			} else {
@@ -41,17 +41,16 @@ public class TresEnRaya {
 			}
 			
 
-			input.close();
-
 			for (int i = 9; i >= 0; i--) {
 				for (int j = 3; j >= 0; j--) {
-					if (Character.toLowerCase(board[j][i]) == '\0') {
+					/*if (Character.toLowerCase(board[j][i]) == '\0') {
 						System.out.print(" _ ");
 					} else if (Character.toLowerCase(board[j][i]) == 'o') {
 						System.out.print(" O ");
 					} else if (Character.toLowerCase(board[j][i]) == 'x') {
 						System.out.print(" X ");
-					}
+					}*/
+					System.out.print(board[j][i]);
 				}
 				System.out.println();
 			}
