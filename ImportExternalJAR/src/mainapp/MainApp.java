02ac4337901ac5a41ffa76eef8b7001b74a6ab16
ShAd0W20 <shadow.jocs@gmail.com>
2022-01-25 16:13:18 +0100

    Import External JARs

---
+++ ImportExternalJAR/src/mainapp/MainApp.java
@@ -0,0 +1,13 @@
+package mainapp;
+import biblioteca.geometria.*;
+import biblioteca.matematiques.operacions.*;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println(Quadrat.area(4));
+		System.out.println(Enters.suma(1, 5));
+
+	}
+
+}
